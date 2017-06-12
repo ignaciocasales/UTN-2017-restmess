70c8158b7b20c4d2fba7213d2f7e9b8b4909ddcf
ignaciocasales <casales.ignacio@gmail.com>
2017-06-12 15:11:37 -0300

    Tests 2

--- src/test/java/com/utn/restmess/controllers/IndexControllerTest.java
+++ src/test/java/com/utn/restmess/controllers/IndexControllerTest.java
@@ -7,19 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,51 +32,15 @@ public class IndexControllerTest {
     private MediaType contentType = new MediaType(MediaType.TEXT_PLAIN.getType(),
             MediaType.TEXT_PLAIN.getSubtype(),
             Charset.forName("utf8"));
-    /*private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));*/
 
     private MockMvc mockMvc;
-
-    private String userName = "bdussault";
-
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
-
-    /*private Account account;
-
-    private List<Bookmark> bookmarkList = new ArrayList<>();
-
-    @Autowired
-    private BookmarkRepository bookmarkRepository;*/
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    /*@Autowired
-    private AccountRepository accountRepository;*/
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-                .findAny()
-                .orElse(null);
-
-        assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
-
     @Before
     public void setup() throws Exception {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
-
-        /*this.bookmarkRepository.deleteAllInBatch();
-        this.accountRepository.deleteAllInBatch();
-
-        this.account = accountRepository.save(new Account(userName, "password"));
-        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + userName, "A description")));
-        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + userName, "A description")));*/
     }
 
     @Test
@@ -91,5 +49,4 @@ public class IndexControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(contentType));
     }
-
 }
